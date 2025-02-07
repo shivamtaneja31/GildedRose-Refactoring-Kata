47dabfd181d4b6c331ebb92d37f0d1b487870fbd
shivamtaneja31 <shivam.taneja@nitorinfotech.com>
2025-02-07 22:34:03 +0530

    Added test case for the changes

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import unittest
-
 from gilded_rose import Item, GildedRose
 
 
@@ -9,8 +8,22 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)  # Fixed the assertion to check the name
 
-        
+    def test_conjured_item(self):
+        items = [Item("Conjured Mana Cake", 3, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 4)  # Conjured items degrade by 2
+        self.assertEqual(items[0].sell_in, 2)
+
+    def test_conjured_item_after_sell_in(self):
+        items = [Item("Conjured Mana Cake", 0, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 2)  # Conjured items degrade by 2 after sell_in
+        self.assertEqual(items[0].sell_in, -1)
+
+
 if __name__ == '__main__':
     unittest.main()
